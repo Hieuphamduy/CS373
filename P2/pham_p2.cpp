00fb89386710692c9337be20f177eb37c86b2308
Hieu Pham <hpham3@binghamton.edu>
2020-10-10 11:23:12 -0400

    3

--- P2/pham_p2.cpp
+++ P2/pham_p2.cpp
@@ -16,102 +16,96 @@ int main( int argc, char* argv[]){
 		gate[i] = new Gate();
 	}
 	string input, side;
+	side = argv[1];
+	input = argv[2];
 	Gate* current = new Gate();	
-	cout << "Enter your input here (or 'Q' to quit): ";
-	cin >> side;	
-	while(side != "Q"){
-		cin >> input;
-		int count = input.length();
-		int index ;
-		cout << side;
-		for(int i = 0; i < count ; ++i){
-			for(int j = 0; j < 4 ; ++j){
-				gate[j] -> dir = side[j];
-			}
-			current = gate[0];
-			if(input[i] == '0'){
-				if(current == gate[0]){
-					char temp = current -> dir;
-					if(temp == 'L'){
-						gate[0] -> dir = 'R';
-						current = gate[1];
-					}
-					else if(temp == 'R'){
-						gate[0] -> dir = 'C';
-						current = gate[3];
-					}
-					else if(temp = 'C'){
-						gate[0] -> dir = 'L';
-						current = gate[2];
-					}	
+	int count = input.length();
+	int index ;
+	cout << side;
+	for(int i = 0; i < count ; ++i){
+		for(int j = 0; j < 4 ; ++j){
+			gate[j] -> dir = side[j];
+		}
+		current = gate[0];
+		if(input[i] == '0'){
+			if(current == gate[0]){
+				char temp = current -> dir;
+				if(temp == 'L'){
+					gate[0] -> dir = 'R';
+					current = gate[1];
 				}
-
-			}
-			else if(input[i] == '1'){
-				if(current == gate[0]){
-					char temp = current -> dir;
-					if(temp == 'L'){
-						gate[0] -> dir = 'C';
-						current = gate[1];
-					}
-					else if(temp == 'R'){
-						gate[0] -> dir = 'L';
-						current = gate[3];
-					}
-					else if(temp = 'C'){
-						gate[0] -> dir = 'R';
-						current = gate[2];
-					}
+				else if(temp == 'R'){
+					gate[0] -> dir = 'C';
+					current = gate[3];
+				}
+				else if(temp = 'C'){
+					gate[0] -> dir = 'L';
+					current = gate[2];
 				}	
-
-			}		
-			if(current == gate[1]){
-				char temp = current -> dir;
-				if(temp == 'L'){
-					gate[1] -> dir = 'R';
-					current = gate[4];
-				}
-				if(temp == 'R'){
-					gate[1] -> dir = 'L';
-					current = gate[5];
-				}
 			}
-			if(current == gate[2]){
-				char temp = current -> dir;
-				if(temp == 'L'){
-					gate[2] -> dir = 'R';
-					current = gate[5];
-				}
-				if(temp == 'R'){
-					gate[2] -> dir = 'L';
-					current = gate[6];
-				}
-			}
-			if(current == gate[3]){
-				char temp = current -> dir;
-				if(temp == 'L'){
-					gate[3] -> dir = 'R';
-					current = gate[6];
-				}
-				if(temp == 'R'){
-					gate[3] -> dir = 'L';
-					current = gate[7];
-				}
-			}
-			for(int k = 0; k < 4; ++k){
-				side[k] = gate[k] -> dir;
-			}
-			cout << " -> " << side;
 
 		}
-		cout << '\t';
-		if(current == gate[4]) cout << 'B';
-		else if(current == gate[5]) cout << 'C';
-		else if(current == gate[6]) cout << 'D';
-		else if(current == gate[7]) cout << 'E';
-		cout << endl;	
-		cout << "Enter your input here (or 'Q' to quit): ";
-		cin >> side;	
+		else if(input[i] == '1'){
+			if(current == gate[0]){
+				char temp = current -> dir;
+				if(temp == 'L'){
+					gate[0] -> dir = 'C';
+					current = gate[1];
+				}
+				else if(temp == 'R'){
+					gate[0] -> dir = 'L';
+					current = gate[3];
+				}
+				else if(temp = 'C'){
+					gate[0] -> dir = 'R';
+					current = gate[2];
+				}
+			}	
+
+		}		
+		if(current == gate[1]){
+			char temp = current -> dir;
+			if(temp == 'L'){
+				gate[1] -> dir = 'R';
+				current = gate[4];
+			}
+			if(temp == 'R'){
+				gate[1] -> dir = 'L';
+				current = gate[5];
+			}
+		}
+		if(current == gate[2]){
+			char temp = current -> dir;
+			if(temp == 'L'){
+				gate[2] -> dir = 'R';
+				current = gate[5];
+			}
+			if(temp == 'R'){
+				gate[2] -> dir = 'L';
+				current = gate[6];
+			}
+		}
+		if(current == gate[3]){
+			char temp = current -> dir;
+			if(temp == 'L'){
+				gate[3] -> dir = 'R';
+				current = gate[6];
+			}
+			if(temp == 'R'){
+				gate[3] -> dir = 'L';
+				current = gate[7];
+			}
+		}
+		for(int k = 0; k < 4; ++k){
+			side[k] = gate[k] -> dir;
+		}
+		cout << " -> " << side;
 	}
+	cout << '\t';
+	if(current == gate[4]) cout << 'B';
+	else if(current == gate[5]) cout << 'C';
+	else if(current == gate[6]) cout << 'D';
+	else if(current == gate[7]) cout << 'E';
+	cout << endl;
 	return 0;
 }
